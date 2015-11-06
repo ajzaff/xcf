c67134a84af668d9cef8ed5f8117a4dcca912564
Lars <lars.scheme@gmail.com>
2015-11-06 17:29:27 +0100

    Added an example.

---
+++ examples/layer_splitter/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonutz/xcf"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("please provide an XCF as the first parameter")
+		return
+	}
+
+	canvas, err := xcf.LoadFromFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	for _, layer := range canvas.Layers {
+		saveImageAsPng(layer, "./"+layer.Name+"_layer.png")
+	}
+
+	saveImageAsPng(composeLayers(canvas), "./composed_layer.png")
+}
+
+func saveImageAsPng(img image.Image, path string) {
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	png.Encode(file, img)
+}
+
+func composeLayers(canvas xcf.Canvas) image.Image {
+	// create an image of the canvas' size
+	img := image.NewRGBA(image.Rect(0, 0, int(canvas.Width), int(canvas.Height)))
+
+	// fill it with a transparent background
+	alpha := image.NewUniform(color.RGBA{0, 0, 0, 0})
+	draw.Draw(img, img.Bounds(), alpha, image.ZP, draw.Src)
+
+	// starting with the bottom-most layer, draw all visible layers over the
+	// image, with their respective opacity
+	for i := len(canvas.Layers) - 1; i >= 0; i-- {
+		layer := canvas.Layers[i]
+		if layer.Visible {
+			opacity := image.NewUniform(color.RGBA{0, 0, 0, layer.Opacity})
+			draw.DrawMask(img, layer.Bounds(), layer, layer.Bounds().Min, opacity, image.ZP, draw.Over)
+		}
+	}
+	return img
+}
